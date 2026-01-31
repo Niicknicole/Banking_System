a97a3934ddb4fa78f0a0e2ea4a98f29580c4b775
Nicole Oliveira <nicoledeoliveiramatos@gmail.com>
2026-01-30 22:54:12 -0300

    Fix: renamed

--- src/test/java/bancking_system/BankingSystemApplicationTests.java
+++ src/test/java/bancking_system/BankingSystemApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BanckingSystemApplicationTests {
+class BankingSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
